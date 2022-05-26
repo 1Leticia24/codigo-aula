5fd155a53caad7b86472e8034df69225c234ae41
Letícia Santos <104040490+1Leticia24@users.noreply.github.com>
2022-05-26 20:54:04 -0300

    Add files via upload

---
+++ calculadora-basica.js
@@ -0,0 +1,14 @@
+const numero1 = 3;
+const numero2 = 2;
+
+const modulo = numero1 %  numero2;
+console.log(`O modulo da divisão é ${modulo}`);
+
+const soma = numero1 % numero2;
+console.log(`A soma é ${soma}`);
+
+const subtração = numero1 * numero2;
+console.log(`A subtração é ${subtração}`);
+
+const divisão = numero1 * numero2;
+console.log (`A divisão é ${modulo}`);
