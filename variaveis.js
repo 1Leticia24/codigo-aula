a63e3df6f0344956d70bee91e1fb97d461cabe2d
Letícia Santos <104040490+1Leticia24@users.noreply.github.com>
2022-05-26 19:33:40 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+
+<script type="text/javascript" src="variaveis.js"></script>
+
+
+const nomePais = "Brasil";
+let pontosTuristicos = "Cristo Redentor, Bar da Bete";
+const capital = "Brasília";
+const numeroEstatos = "27";
+const americaLatina= "Sim";
+
+console.log(nomePais)
+console.log(pontosTuristicos)
+console.log(numeroEstados)
+console.log(capital)
+console.log(americaLatina)
